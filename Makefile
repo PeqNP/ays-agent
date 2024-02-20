23a0282b1890b1d9c7501f0839717b844a809854
Eric Chamberlain <eric.j.chamberlain@protonmail.com>
2024-02-20 13:24:11 -0800

    Updated license. Can now build and install the project locally. Pin urllib3 to v1.26. Apparently v2 isn't supported with openssl.

--- Makefile
+++ Makefile
@@ -4,4 +4,10 @@ init:
 test:
 	pytest -vv --log-cli-level Debug ./tests
 
-.PHONY: init test
+build:
+	python3 setup.py sdist bdist_wheel
+
+install:
+	pip3 install dist/ays_agent-1.0-py3-none-any.whl --force-reinstall
+
+.PHONY: init test build install
---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#
+# Setup the ays-agent to be used as a stand-alone executable
+#
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ays_agent',
+    version='1.0',
+    authors = [
+        {"name": "Eric Chamberlain", "email": "bitheadrl@protonmail.com"}
+    ],
+    description="@ys Agent",
+    readme="README.md",
+    requires_python=">=3.8",
+    install_requires=[
+        "click>=7.1.2",
+        "fastapi>=0.109.1",
+        "fastapi-utils>=0.2.1",
+        "PyYaml>=6.0",
+        "requests>=2.31.0",
+        "rich>=13.7.0",
+        "typer>=0.9.0",
+        # urllib3 v2 not supported
+        "urllib3==1.26.18",
+        "uvicorn>=0.18.3"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'ays-agent = ays_agent.cli:app',
+        ],
+    },
+)

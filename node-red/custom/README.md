1302f2c0826dbe31a0c5499534f8a0c9bca5b342
Eric Chamberlain <eric.j.chamberlain@protonmail.com>
2024-03-04 14:01:11 -0800

    These are now part of the node-red-ays-agent. Required for publishing npm packages.

--- node-red/custom/README.md
+++
@@ -1,5 +0,0 @@
-# README
-
-These nodes should not be used at this time.
-
-These files may be the next `ays-agent` node. A subflow will be used for now.
--- node-red/custom/ays-agent-server.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(RED) {
-    function AYSAgentServerNode(n) {
-        RED.nodes.createNode(this, n);
-        this.endpoint = n.endpoint;
-        this.orgSecret = n.orgSecret;
-    }
-    RED.nodes.registerType("ays-agent-server", AYSAgentServerNode);
-}
